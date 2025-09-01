f7b9e4f64bd17dd99b406a1eee23e919b9cd255d
Rouzihiro <ryossj@gmail.com>
2025-09-01 11:09:49 +0200

    patch: centered

--- config.h
+++ config.h
@@ -4,9 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;		/* -c option; centers dmenu on screen - CHANGED TO 1 */
 static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
-static int min_width = 400;		/* minimum width when centered */
+static int min_width = 500;		/* minimum width when centered */
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
-
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+ 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true",
